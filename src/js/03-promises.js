5c2e64fe2c281072b29486df4d3402cd15ab6884
Erika Tatchyn <erika.ucu@gmail.com>
2023-01-12 16:16:32 +0000

    add task 3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,29 @@
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+  return new Promise((resolve, reject) => {
+    const shouldResolve = Math.random() > 0.3;
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
 }
+
+document.querySelector('.form').addEventListener('submit', e => {
+  e.preventDefault();
+  const delay = e.target.elements.delay.value;
+  const step = e.target.elements.step.value;
+  const amount = e.target.elements.amount.value;
+  for (let i = 0; i < amount; i++) {
+    const promiseDelay = Number(delay) + Number(step) * i;
+    createPromise(i, promiseDelay)
+      .then(res => {
+        console.log('Fulfilled: ', res);
+      })
+      .catch(error => {
+        console.log('Rejected: ', error);
+      });
+  }
+});
