f620b3489833c3c2414de68f3815a5b97e64f475
Erika Tatchyn <erika.ucu@gmail.com>
2023-01-19 14:47:27 +0000

    finish js 10

--- src/index.js
+++ src/index.js
@@ -1,30 +1,55 @@
 import './css/styles.css';
 import { fetchCountries } from './fetchCountries';
+import Notiflix from 'notiflix';
 const debounce = require('lodash.debounce');
 const DEBOUNCE_DELAY = 300;
 const searchInput = document.querySelector('#search-box');
 const countryList = document.querySelector('.country-list');
+const countryInfo = document.querySelector('.country-info');
 
 const handleSearch = debounce(value => {
   fetchCountries(value)
     .then(countries => {
-      countryList.innerHTML = '';
-      for (let country of countries) {
-        console.log(country);
-        const li = document.createElement('li');
-        li.innerHTML = `
+      if (countries.length > 10) {
+        Notiflix.Notify.failure(
+          'Too many matches found. Please enter a more specific name.'
+        );
+        return;
+      }
+      if (countries.length === 1) {
+        countryList.innerHTML = '';
+        const country = countries[0];
+        countryInfo.innerHTML = `
         <h3>${country.name.official}</h3>
         <p>Capital: ${country.capital}</p>
         <p>Population: ${country.population}</p>
         <img src=${
           country.flags.svg
-        } alt=${`Flag of ${country.name.official}`} />
-        <p>Languages: ${Object.values(country.languages)}</p>
+        } alt=${`Flag of ${country.name.official}`} width="50px" height="auto" />
+        <p>Languages: ${Object.values(country.languages).join(', ')}</p>
       `;
-        countryList.appendChild(li);
+      } else if (countries.length >= 2) {
+        countryInfo.innerHTML = '';
+        countryList.innerHTML = '';
+        for (let country of countries) {
+          const li = document.createElement('li');
+          li.innerHTML = `
+            <img src=${
+              country.flags.svg
+            } alt=${`Flag of ${country.name.official}`} width="50px" height="auto" />
+            <span>${country.name.official}</span>
+          `;
+          countryList.appendChild(li);
+        }
+      } else {
+        countryList.innerHTML = '';
+        countryInfo.innerHTML = '';
+        Notiflix.Notify.Failure('Oops, there is no country with that name.');
       }
     })
-    .catch(error => console.log(error));
+    .catch(error => {
+      Notiflix.Notify.failure('Oops, there is no country with that name.');
+    });
 }, DEBOUNCE_DELAY);
 
 searchInput.addEventListener('input', e => {
